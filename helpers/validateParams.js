b186da8c26cd6d7dd3f5b9e2b89f7772a28c05af
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-15 15:18:04 +0300

    fixed

--- helpers/validateParams.js
+++ helpers/validateParams.js
@@ -1,11 +1,7 @@
-const validateParams = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.params);
-    if (error) {
-      return res.status(400).json({ message: error.details.map(detail => detail.message).join(", ") });
-    }
-    next();
-  };
+const validateParams = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.params);
+  if (error) return res.status(400).json({ message: error.details.map(detail => detail.message).join(", ") });
+  next();
 };
 
 export default validateParams;
