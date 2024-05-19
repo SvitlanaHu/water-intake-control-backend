ad6a8c11bd9277ac8d19fcf2ec481a7c8e0f5bc0
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-19 12:07:35 +0300

    Update usersControllers.js

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -172,3 +172,13 @@ export const refreshUserTokens = async (req, res, next) => {
   }
 };
 
+export const countUniqueUsers = async (req, res) => {
+  try {
+    const uniqueUsersCount = await User.countDocuments();
+    res.json({ total: uniqueUsersCount });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
